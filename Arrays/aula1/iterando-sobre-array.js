0f23b9998bad3c511cd2d502d3050972b64272d4
Aldair André <aldairandre99@gmail.com>
2021-07-23 13:14:16 +0100

    Iteracao com laco for em um array

---
+++ Arrays/aula1/iterando-sobre-array.js
@@ -0,0 +1,9 @@
+const mochilaDeTrabalho = [
+    'computador','carregador do computador', 'agua', 'escova'
+]
+
+//Com laço for
+
+for(let c = 0;mochilaDeTrabalho.length > c; c++){
+    console.log(mochilaDeTrabalho[c]);
+}
