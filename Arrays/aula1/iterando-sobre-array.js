fdc0e37e16e268ab29cb98269b371f892ee269c0
Aldair André <aldairandre99@gmail.com>
2021-07-23 14:28:59 +0100

    itercacao com array usando laco for in

--- Arrays/aula1/iterando-sobre-array.js
+++ Arrays/aula1/iterando-sobre-array.js
@@ -2,8 +2,22 @@ const mochilaDeTrabalho = [
     'computador','carregador do computador', 'agua', 'escova'
 ]
 
+const estilo = () => {
+    console.log("=".repeat(20) + "\n")
+};
+
 //Com laço for
+
+console.log("\t FOR");
+estilo();
 
 for(let c = 0;mochilaDeTrabalho.length > c; c++){
     console.log(mochilaDeTrabalho[c]);
+}
+console.log("\n \t FOR IN");
+estilo();
+//for in
+
+for (let itens in mochilaDeTrabalho){
+    console.log(mochilaDeTrabalho[itens]);
 }
