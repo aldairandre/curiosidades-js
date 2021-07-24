e709c90e8caf236c069ff1f528ffbfc2c3d5d696
Aldair André <aldairandre99@gmail.com>
2021-07-24 04:47:17 +0100

    iteracao sobre um array com forEach

--- Arrays/aula1/iterando-sobre-array.js
+++ Arrays/aula1/iterando-sobre-array.js
@@ -2,9 +2,7 @@ const mochilaDeTrabalho = [
     'computador','carregador do computador', 'agua', 'escova'
 ]
 
-const estilo = () => {
-    console.log("=".repeat(20) + "\n")
-};
+const estilo = () => console.log("=".repeat(20) + "\n");
 
 //Com laço for
 
@@ -21,3 +19,35 @@ estilo();
 for (let itens in mochilaDeTrabalho){
     console.log(mochilaDeTrabalho[itens]);
 }
+
+console.log("");
+
+console.log("FOREACH")
+
+//FORFOREACH
+
+estilo()
+
+const numero = [1,2,3,4,5,6];
+
+/*  
+    FOREACH()
+    Para usar este medo necessita de uma função de callback ou função anonima ou então uma arrow function
+    
+    Sintaxe
+        array.forEach(value, index, array)
+*/
+
+/*
+Ex1
+
+ * function out(value, index, array){
+    console.log(array);
+}
+
+numero.forEach(out)
+*/
+
+//Ex2
+
+numero.forEach((index,value,array) => console.log(array))
